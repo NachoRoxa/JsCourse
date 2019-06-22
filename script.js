da062629c4c507588683f7e09d822385eac4a7cd
NachoRoxa <ig.rocha.pavez@gmail.com>
2019-06-22 13:34:24 -0400

    Resolving Code challenge 2 and more topics

--- script.js
+++ script.js
@@ -127,7 +127,7 @@ BMI= mass / height^2=mass / (height * height).
 // var BMI = markBMI > johnBMI;
 // console.log('Is Mark\'s BMI greater than John\'s? ' + BMI);
 
-/* 
+/*
 If / else Statements
 */
 
@@ -168,13 +168,134 @@ If / else Statements
 
 /*Boolean Logic */
 
-var firstName='John';
-var age=16;
+// var firstName = 'John';
+// var age =20;
 
-if(age<13){
-    console.log(firstName+' is a boy.');
-}else if(age>=13 && age<18){
-    console.log(firstName+' is a teen.');
-}else{
-    console.log(firstName+' is a man.');
-}
+// if (age < 13) {
+//     console.log(firstName + ' is a boy.');
+// } else if (age >= 13 && age < 20) {
+//     console.log(firstName + ' is a teen.');
+// } else if(age => 20 && age <30){
+//     console.log(firstName+' is a young man.');
+// } else {
+//     console.log(firstName + ' is a man.');
+// }
+
+/*The ternary Operator and Switch Statements */
+
+// var firstName = 'Nacho';
+// var age = 16;
+
+// //Ternary Operator
+// age >= 18 ? console.log(firstName + ' drinks beer.')
+//     : console.log(firstName + ' drinks juice.');
+
+// var drink = age >= 18 ? 'beer' : 'juice';
+// console.log(drink);
+
+// Same as above
+// if(age>=18){
+//     var drink='beer';
+// } else{
+//     var drink='juice';
+// }
+
+// Switch Statement
+
+// var job = 'teacher';
+// switch (job) {
+//     case 'teacher':
+//         console.log(firstName + ' teaches kids how to code');
+//         break;
+//     case 'driver':
+//         console.log(firstName + ' drive an uber.');
+//         break;
+//     case 'designer':
+//         console.log(firstName + ' like to draw fashion');
+//         break;
+//     default:
+//         console.log(firstName + ' does nothing for his/her life');
+// }
+
+// switch (true) {
+//     case age < 13:
+//         console.log(firstName + ' is a boy.');
+//         break;
+//     case age >= 13 && age < 20:
+//         console.log(firstName + ' is a teen.');
+//         break;
+//     case age => 20 && age < 30:
+//         console.log(firstName + ' is a young man.');
+//         break;
+//     default:
+//         console.log(firstName + ' is a man.');
+// }
+
+/*Truthy and Falsy Values */
+
+/*Falsy are values that will be converted to false after been evaluated in
+a true/false condition this values are: undefined, null, 0, '', NaN
+Truthy values are values that are considered true after an true/flase condition
+this values are: NOT falsy values*/
+
+// var height;
+
+// height=23;
+
+// if(height){
+//     console.log('variable is defined');
+// }else{
+//     console.log('Variable has not been defined');
+// }
+
+// //Equality operator
+// if(height=='23'){
+//     console.log('The == operatro does type coercion!');
+// }
+// if(height==='23'){
+//     console.log('The === does not type coercion!');
+// }
+
+
+/*CODE CHALLENGE 2*/
+/*John and Mike both play basketball in different teams.
+In the latest 3 games, John's team scored 89,120 and 103 points,
+while Mike's team scored 116, 94 and 123 points.
+
+1. Calculate the average score for each team.
+2. Decide which team wins in average(highest average score), and
+print the winner to the console. Also include the average socre in
+the output.
+3. Then change the score to show different winners.
+Don't forget to take into account there might be a draw.
+
+4. EXTRA: Mary also plays basketball, and her team scored 97, 134
+and 105 points. Like before, log the average winner to the console.
+HINT: you will need the && operator to take the decision.*/
+
+// var averageJohnTeam = (89 + 120 + 103) / 3;
+// var averageMikeTeam = (116 + 94 + 123) / 3;
+// var averageMaryTeam = (97 + 134 + 105) / 3;
+// winner = true;
+
+// switch (winner) {
+//     case averageJohnTeam > averageMikeTeam && 
+//          averageJohnTeam > averageMaryTeam:
+//         console.log('John\'s team is the winner with an average'
+//             + ' score of ' + averageJohnTeam);
+//         break;
+//     case averageMikeTeam > averageJohnTeam && 
+//          averageMikeTeam > averageMaryTeam:
+//         console.log('Mike\'s team is the winner with an average'
+//             + ' score of ' + averageMikeTeam);
+//         break;
+//     case averageMaryTeam > averageJohnTeam &&
+//          averageMaryTeam > averageMikeTeam:
+//         console.log('Mary\'s team is the winner with an average'
+//             + ' score of ' + averageMaryTeam);
+//         break;
+//     case averageJohnTeam===averageMikeTeam &&
+//          averageJohnTeam===averageMaryTeam:
+//         console.log('There is a Draw!!');
+//         break;
+// }
