de5115c1a7fa95cd7336df3d564cf3d355021e46
NachoRoxa <ig.rocha.pavez@gmail.com>
2019-06-23 14:48:27 -0400

    Coding challenge 3 and last changes on this file just because I hate more than 400 lines

--- script.js
+++ script.js
@@ -335,20 +335,93 @@ HINT: you will need the && operator to take the decision.*/
 // }
 
 //Function Expression
-var whatDoYouDo = function (job, firstName) {
-    switch (job) {
-        case 'teacher':
-            return firstName + ' teaches kids how to code';
-        case 'driver':
-            return firstName + ' drives an uber car';
-        case 'designer':
-            return firstName + ' draws and make web page desing';
-        default:
-            return firstName + ' does something else';
-    }
-}
+// var whatDoYouDo = function (job, firstName) {
+//     switch (job) {
+//         case 'teacher':
+//             return firstName + ' teaches kids how to code';
+//         case 'driver':
+//             return firstName + ' drives an uber car';
+//         case 'designer':
+//             return firstName + ' draws and make web page desing';
+//         default:
+//             return firstName + ' does something else';
+//     }
+// }
 
-console.log(whatDoYouDo('teacher', 'John'));
-console.log(whatDoYouDo('designer', 'Jane'));
-console.log(whatDoYouDo('retired', 'Mike'));
+// console.log(whatDoYouDo('teacher', 'John'));
+// console.log(whatDoYouDo('designer', 'Jane'));
+// console.log(whatDoYouDo('retired', 'Mike'));
 
+/**ARRAYS */
+
+//Initializa new array
+// var names = ['John', 'Mark', 'Jane'];
+// var years = new Array(1990, 1969, 1948);
+
+// console.log(names[0]);
+
+// //mutate array data
+// names[1] = 'Ben';
+// names[names.length] = 'Mary';
+// console.log(names);
+
+// //Different data types
+
+// var john = ['John', 'Smith', 1990, 'teacher', false];
+// john.push('blue');
+// john.unshift('Mr.');
+
+// console.log(john);
+
+// john.pop(); //removes the last element of an Array.
+// john.pop();
+// john.shift(); //removes the first element of an Array.
+// console.log(john);
+
+// console.log(john.indexOf(23));
+
+// var isDesigner = john.indexOf('designer')===-1?'John is not a designer':
+// 'John is a designer';
+// console.log(isDesigner);
+
+/*CODE CHALLENGE 3 */
+
+/*
+John and his family went on a holiday and went to 3 different restaurants.
+The bills were $124, $48 and $268.
+To tip the waiter a fair amount, John created a simple tip calculator.
+He likes to tip 20% of the bill when the bill is less than $50, 15% when
+the bill is between $50 and $200, and 10% if the bill is more than $200-
+
+In the end, John would like to have 2 arrays:
+1) Containing all three tip (one for each bill).
+2) Containing all three final paid amount (bill + tip).
+*/
+
+// var bills = [124, 48, 268];
+
+// function tipCalculator(bill) {
+//     var percentage;
+//     if (bill < 50) {
+//         percentage = 0.2;
+//     } else if (bill >= 50 && bill < 200) {
+//         percentage = 0.15;
+//     } else {
+//         percentage = 0.1;
+//     }
+//     return percentage * bill;
+// }
+
+// var tips = [
+//     tipCalculator(bills[0]),
+//     tipCalculator(bills[1]),
+//     tipCalculator(bills[2])
+// ];
+
+// var finalAmout = [
+//     bills[0] + tips[0],
+//     bills[1] + tips[1],
+//     bills[2] + tips[2]
+// ];
+
+// console.log(tips, finalAmout);
