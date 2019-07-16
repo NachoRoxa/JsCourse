fa9522773ec910e9743ff83bf0219445fd3cd581
NachoRoxa <ig.rocha.pavez@gmail.com>
2019-07-16 00:10:34 -0400

    commit pre eslint installation

--- Section3/jsCourseScript.js
+++ Section3/jsCourseScript.js
@@ -1,34 +1,37 @@
-/**
- * This file is the first of the Section 3 of the JsCourse
- */
+// /**
+//  * This file is the first of the Section 3 of the JsCourse
+//  */
 
-/**
- * Lecture: Hoisting
- */
+// /**
+//  * Lecture: Hoisting
+//  */
 
-calculateAge(1965);
+// calculateAge(1965);
 
-function calculateAge(year) {
-    console.log(2016 - year);
-}
+// function calculateAge (year) {
+//   console.log(2016 - year);
+// }
 
-//retirement(1990);
-//Function expression doesn't work in Hoisting
-var retirement = function (year) {
-    console.log(65 - (2016 - year));
-}
+// // retirement(1990);
+// // Function expression doesn't work in Hoisting
+// var retirement = function (year) {
+//   console.log(65 - (2016 - year));
+// }
 
-//variables
-console.log(age);// It's undefined 
-var age = 23;
-console.log(age);
+// // variables
+// console.log(age);// It's undefined
+// var age = 23;
+// console.log(age);
 
+// function foo () {
+//   var age = 65;
+//   console.log(age);// variable in a different context, only for the function
+//   // this one print 65 it's because it takes the value of the variable in the function
+// }
 
-function foo(){
-    var age=65;
-    console.log(age); //variable in a different context, only for the function
-    //this one print 65 it's because it takes the value of the variable in the function
-}
+// foo();
+// console.log(age); // this one takes the value of the global context
 
-foo();
-console.log(age);//this one takes the value of the global context.
+// First scoping example
+
+var a='Hello!';
