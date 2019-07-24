379c1f1f0b5f832d305a1771204b1313165bc27f
NachoRoxa <ig.rocha.pavez@gmail.com>
2019-07-24 13:13:54 -0400

    Creating functions and improving the game.

--- Section2/jsCourseScript2.js
+++ Section2/jsCourseScript2.js
@@ -1,15 +1,15 @@
-/*Objects and Properties */
+/* Objects and Properties */
 /**
  * inside a object you can store multiple types of values
  even another object or more than one object.
  To access the any propertie of the object you have to call
  the object with the dot notation. Ex: object.propertie
  or calling the object and the key of the object.
- The Key of the object is the "variable name" that you assign 
+ The Key of the object is the "variable name" that you assign
  in the object declaration.
  */
 
-/*Object Literal */
+/* Object Literal */
 // var john = {
 //     firstName: 'John',
 //     lastName: 'Smith',
@@ -60,14 +60,14 @@
 // john.calcAge()
 // console.log(john)
 
-/*CODING CHALLENGE 4 */
+/* CODING CHALLENGE 4 */
 
 /**
 Let's remember the first coding challenge where Mark and John compared their BMIs.
 Let's now implement the same functionality with objects and methods.
 1. For each of them, create an object with properties for their full name, mass,
 and height.
-2. Then, add a method to each object to calculate the BMI. Save the BMI to 
+2. Then, add a method to each object to calculate the BMI. Save the BMI to
 the object and also return it from the method.
 3. In the end, log to the console who has the highest BMI, together with
 the full name abd tge resoectuve BMI. Don't forget thay might have the same BMI.
@@ -119,10 +119,10 @@ Remember: BMI=mass/height^2=mass/(height*height) */
 // }
 
 /**
-* Loops and iteration 
+* Loops and iteration
 */
 
-//For loop
+// For loop
 // for (var i = 0; i < 10; i++) {
 //     console.log(i);
 // }
@@ -140,7 +140,7 @@ Remember: BMI=mass/height^2=mass/(height*height) */
 //     i++;
 // }
 
-//continue and break statements
+// continue and break statements
 
 // var john = ['John', 'Smith', 1991, 'designer', false];
 
@@ -169,22 +169,22 @@ Remember: BMI=mass/height^2=mass/(height*height) */
  * This time, John and his family went to 5 different restaurant.
  * The bills were $124, $48, $268, $180 and $42-
  * John likes to tip 20% when the bill is less than $50, 15% when the bill
- * is between $50 and $200, and 10%if the bill is more than $200-
- * 
+ * is between $50 and $200, and 10%if the bill is more than $200
+ *
  * Implement a tip calculator using object and loops:
  * 1.- Create an object with an array for the bill values
  * 2.- Add a method to calculate the tip.
- * 3.- This method should include a loop to iterate over all 
+ * 3.- This method should include a loop to iterate over all
  * the paid bills and do the tip calculations.
  * 4.- As an output, create 1) a new array containing all tips,
  * and 2) an array containing final paid amounts (bill+tip).
- * 
+ *
  * EXTRA AFTER FINISHING: Mark's family also wetn on a holiday going to
  * 4 different restaurants. The bills were $77, $375, $110 and $45.
  * Mark likes to tip 20% of the bill when the bill is less than $100,
  * 10% when the bill is between $100 and $300, and 25% if the bill is more than $300
  * (different than John).
- * 
+ *
  * 5.- Implement the same funcionality as before. this time using Mark's tipping rules
  * 6.- Crea a function (not a method) to calculate the average of a given array of tips.
  * Hint: Loop over the array, and in each iteration store the current sum in
@@ -194,63 +194,63 @@ Remember: BMI=mass/height^2=mass/(height*height) */
  * 8.- Log to the console which family paid the highest tips on average.
  */
 
-var johnBills = {
-    bills: [124, 48, 268, 180, 42],
-    tips: [],
-    finalAmount: [],
-    tipCalculator: function () {
-        for (i = 0; i < this.bills.length; i++) {
-            if (this.bills[i] < 50) {
-                this.tips.push(this.bills[i] * 0.2);
-                this.finalAmount.push(this.bills[i] + this.tips[i]);
-            } else if (this.bills[i] >= 50 && this.bills[i] < 200) {
-                this.tips.push(this.bills[i] * 0.15);
-                this.finalAmount.push(this.bills[i] + this.tips[i]);
-            } else {
-                this.tips.push(this.bills[i] * 0.1);
-                this.finalAmount.push(this.bills[i] + this.tips[i]);
-            }
-        }
-    }
-};
+// var johnBills = {
+//     bills: [124, 48, 268, 180, 42],
+//     tips: [],
+//     finalAmount: [],
+//     tipCalculator: function () {
+//         for (i = 0; i < this.bills.length; i++) {
+//             if (this.bills[i] < 50) {
+//                 this.tips.push(this.bills[i] * 0.2);
+//                 this.finalAmount.push(this.bills[i] + this.tips[i]);
+//             } else if (this.bills[i] >= 50 && this.bills[i] < 200) {
+//                 this.tips.push(this.bills[i] * 0.15);
+//                 this.finalAmount.push(this.bills[i] + this.tips[i]);
+//             } else {
+//                 this.tips.push(this.bills[i] * 0.1);
+//                 this.finalAmount.push(this.bills[i] + this.tips[i]);
+//             }
+//         }
+//     }
+// };
 
-var markBills = {
-    bills: [77, 375, 110, 45],
-    tips: [],
-    finalAmount: [],
-    tipCalculator: function () {
-        for (i = 0; i < this.bills.length; i++) {
-            if (this.bills[i] < 100) {
-                this.tips.push(this.bills[i] * 0.2);
-                this.finalAmount.push(this.bills[i] + this.tips[i]);
-            } else if (this.bills[i] >= 100 && this.bills[i] < 300) {
-                this.tips.push(this.bills[i] * 0.1);
-                this.finalAmount.push(this.bills[i] + this.tips[i]);
-            } else {
-                this.tips.push(this.bills[i] * 0.25);
-                this.finalAmount.push(this.bills[i] + this.tips[i]);
-            }
-        }
-    }
-};
+// var markBills = {
+//     bills: [77, 375, 110, 45],
+//     tips: [],
+//     finalAmount: [],
+//     tipCalculator: function () {
+//         for (i = 0; i < this.bills.length; i++) {
+//             if (this.bills[i] < 100) {
+//                 this.tips.push(this.bills[i] * 0.2);
+//                 this.finalAmount.push(this.bills[i] + this.tips[i]);
+//             } else if (this.bills[i] >= 100 && this.bills[i] < 300) {
+//                 this.tips.push(this.bills[i] * 0.1);
+//                 this.finalAmount.push(this.bills[i] + this.tips[i]);
+//             } else {
+//                 this.tips.push(this.bills[i] * 0.25);
+//                 this.finalAmount.push(this.bills[i] + this.tips[i]);
+//             }
+//         }
+//     }
+// };
 
-function averageTips(tip) {
-    var tipSum = 0;
-    for (i = 0; i < tip.length; i++) {
-        tipSum += tip[i];
-    }
-    return tipSum / tip.length;
-}
+// function averageTips(tip) {
+//     var tipSum = 0;
+//     for (i = 0; i < tip.length; i++) {
+//         tipSum += tip[i];
+//     }
+//     return tipSum / tip.length;
+// }
 
-johnBills.tipCalculator();
-markBills.tipCalculator();
-johnBills.average=averageTips(johnBills.tips);
-markBills.average=averageTips(markBills.tips);
+// johnBills.tipCalculator();
+// markBills.tipCalculator();
+// johnBills.average=averageTips(johnBills.tips);
+// markBills.average=averageTips(markBills.tips);
 
-if (markBills.average >johnBills.average) {
-        console.log('Mark\'s family spent an average of: '+
-        averageTips(markBills.tips));
-}else{
-    console.log('John\'s family spent an average of: '+
-        averageTips(johnBills.tips));
-}
+// if (markBills.average >johnBills.average) {
+//         console.log('Mark\'s family spent an average of: '+
+//         averageTips(markBills.tips));
+// }else{
+//     console.log('John\'s family spent an average of: '+
+//         averageTips(johnBills.tips));
+// }
--- Section4/jsSectionScript.js
+++ Section4/jsSectionScript.js
@@ -2,7 +2,7 @@
 var scores, roundScores, activePlayer;
 scores = [0, 0];
 roundScores = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 // document.querySelector('#current-' + activePlayer).textContent = dice;
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
@@ -23,4 +23,50 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
   var diceDom = document.querySelector('.dice');
   diceDom.style.display = 'block';
   diceDom.src = './Section4/dice-' + dice + '.png';
+
+  if (dice !== 1) {
+    roundScores += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScores;
+  } else {
+    nextPlayer();
+  }
 });
+
+document.querySelector('.btn-hold').addEventListener('click', function () {
+  scores[activePlayer] += roundScores;
+  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+  nextPlayer();
+
+  if (scores[activePlayer] >= 100) {
+    document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+  } else {
+    nextPlayer();
+  }
+});
+
+function nextPlayer () {
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  /**
+   * This is the same thing that writing
+   * the following if statement.
+   * if (activePlayer === 0) {
+   * activePlayer = 1;
+   * } else {
+   * activePlayer = 0;
+   * }
+  */
+  roundScores = 0;
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  // document.querySelector('.player-0-panel').classList.remove('active');
+  // document.querySelector('.player-1-panel').classList.add('active');
+
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+
+  document.querySelector('.dice').style.display = 'none';
+}
